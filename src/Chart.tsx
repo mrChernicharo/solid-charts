7fe93162ac318c9e3dffef424d0511d590341767
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2022-10-01 23:53:09 -0400

    transition container works

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -6,7 +6,11 @@ const Chart: Component<{ data: DataPoint[] }> = (props) => {
   const [chartData, setChartData] = createSignal(props.data);
 
   return (
-    <TransitionContainer data={props.data}>
+    <TransitionContainer
+      duration={1000}
+      data={props.data}
+      onUpdate={setChartData}
+    >
       <div style={{ height: "300px", border: "1px solid" }}>
         <h3>Chart</h3>
         <pre>{"data " + JSON.stringify(props.data)}</pre>
--- src/TransitionContainer.tsx
+++ src/TransitionContainer.tsx
@@ -1,18 +1,86 @@
-import { children, Component, createSignal, JSXElement } from "solid-js";
+import {
+  children,
+  Component,
+  createEffect,
+  createSignal,
+  JSXElement,
+} from "solid-js";
 import { DataPoint } from "./App";
+import { useTransitionValue } from "use-transition-value";
 
 const TransitionContainer: Component<{
   children: JSXElement;
   data: DataPoint[];
+  duration: number;
+  onUpdate: (data: DataPoint[]) => void;
 }> = (props) => {
-  const [test, setTest] = createSignal("test");
+  let prevList: DataPoint[] = [];
 
-  return (
-    <div>
-      <div>transition container</div>
-      {props.children}
-    </div>
+  const [data, setData] = createSignal<DataPoint[]>([]);
+  const [transitionList, setTransitionList] = createSignal<DataPoint[]>(
+    props.data
   );
+
+  const updateTransitionList = (curr: number, idx: number) =>
+    setTransitionList((list) =>
+      list.map((d, i) => (i === idx ? { ...d, value: curr } : d))
+    );
+
+  // data setup: receiving bulk data via props
+  createEffect(() => {
+    prevList = data();
+    setData(props.data);
+    setTransitionList(props.data);
+  });
+
+  // transitions setup: tell what values should update when
+  createEffect(() => {
+    // Initial Transition
+    if (prevList.length === 0) {
+      data().forEach((d, idx, arr) => {
+        useTransitionValue({
+          id: String(idx),
+          initial: 0,
+          final: arr[idx].value,
+          duration: props.duration,
+          cb: (val: number) => updateTransitionList(val, idx),
+        });
+      });
+    }
+
+    // new element added
+    if (prevList.length !== 0 && prevList.length < props.data.length) {
+      let idx = props.data.length - 1;
+      // console.log("added new element!", { data: data(), item: data()[idx], idx });
+
+      useTransitionValue({
+        id: String(idx),
+        initial: 0,
+        final: data()[idx].value || 0,
+        duration: props.duration,
+        cb: (val: number) => updateTransitionList(val, idx),
+      });
+    }
+
+    // standalone value updated
+    data().forEach((d, idx, arr) => {
+      if (prevList[idx] && prevList[idx].value !== arr[idx].value) {
+        // console.log("useTransitionValue! value updating!", { prevList, arr, value: arr[idx].value });
+        useTransitionValue({
+          id: String(idx),
+          initial: prevList[idx].value,
+          final: arr[idx].value,
+          duration: props.duration,
+          cb: (val: number) => updateTransitionList(val, idx),
+        });
+      }
+    });
+  });
+
+  createEffect(() => {
+    props.onUpdate(transitionList());
+  });
+  return <>{props.children}</>;
 };
 
 export default TransitionContainer;
